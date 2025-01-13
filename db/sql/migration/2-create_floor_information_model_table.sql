2404cc99d430d17f7d7c0b2698cadbcc4c77c468
karasuneo <yusei4520030401@gmail.com>
2025-01-13 13:53:00 +0900

    add: db内容をWebで閲覧できる出力結果

---
+++ db/sql/migration/2-create_floor_information_model_table.sql
@@ -0,0 +1,22 @@
+-- noqa: disable=all
+\c indoor_location_estimation;
+-- noqa: disable=all
+
+CREATE TABLE coordinates (
+    id SERIAL
+    , x INTEGER NOT NULL
+    , y INTEGER NOT NULL
+    , is_walkable BOOLEAN NOT NULL
+    , created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    , updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    , floor_id VARCHAR(26) REFERENCES floors (id)
+    , PRIMARY KEY (id)
+);
+
+CREATE TABLE coordinate_information (
+    id VARCHAR(26)
+    , created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    , updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    , coordinate_id SERIAL REFERENCES coordinates (id)
+    , PRIMARY KEY (id)
+);
---
+++ db/sql/migration/3-create_walking_information_model_table copy.sql
@@ -0,0 +1,14 @@
+-- noqa: disable=all
+\c indoor_location_estimation;
+-- noqa: disable=all
+
+CREATE TABLE estimated_positions (
+    id VARCHAR(26) PRIMARY KEY
+    , x INTEGER NOT NULL
+    , y INTEGER NOT NULL
+    , is_converged BOOLEAN NOT NULL
+    , direction INTEGER NOT NULL
+    , created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    , trajectory_id VARCHAR(26) REFERENCES trajectories (id)
+    , walking_information_id VARCHAR(26) REFERENCES walking_information (id)
+);
